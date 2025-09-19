d16df9a52f52857e7070aad77f932716fc066d8b
Yusuf Usman <yusuf.usman1003@gmail.com>
2025-09-19 15:44:43 +0100

    update

--- components/about/BioText.tsx
+++ components/about/BioText.tsx
@@ -10,17 +10,17 @@ const BioText = () => {
       </h2>
 
       <p className="text-grey mb-4 leading-relaxed text-sm sm:text-base max-w-lg">
-        Hi i'm Yusuf Usman, a <span className="text-secondary font-semibold">Backend Engineer </span> 
+        Hi i&apos;m Yusuf Usman, a <span className="text-secondary font-semibold">Backend Engineer </span> 
         with strong exertise in java, my core language, and additional experience
         building applications using python and Django. I design and deploy scalable
         microservices using Spring Boot, containerized environments and databases
         ranging from MongoDB to relational systems like PostgreSQL. I enjoy solving
-        challenges around scale and reliability, and i'm looking for roles focused
+        challenges around scale and reliability, and i&apos;m looking for roles focused
         on distributed systems and backend architecture, thank you.
       </p>
 
       <p className="text-grey mb-6 text-sm sm:text-base max-w-lg">
-        With experience in <span className="font-medium">Java, Python, Sui move,   Go, Django, Node.js, Javascript and databases ranging from MongoDB and relational systems like PostgreSQL</span>, 
+        With experience in <span className="font-medium">Java, Python, Sui move, Go, Django, Node.js, Javascript and databases ranging from MongoDB and relational systems like PostgreSQL</span>, 
         I specialize in authentication, cloud deployment, and performance 
         optimization. I enjoy solving complex problems, writing clean code, 
         and collaborating to deliver seamless digital experiences.
